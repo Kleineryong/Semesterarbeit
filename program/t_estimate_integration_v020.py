6cc2dda6b6646e7054e1b87a105d672a2878a7fd
Kleineryong <herr.wang131@outlook.com>
2023-05-31 16:24:38 +0200

    V022_return
    return back to 05.30

--- program/t_estimate_integration_v020.py
+++ program/t_estimate_integration_v020.py
@@ -10,23 +10,16 @@ import multiprocessing as mp
 from scipy.constants import c, h, k
 from scipy.optimize import curve_fit, least_squares
 from scipy.integrate import quad
+from scipy.interpolate import interp1d
 from joblib import Parallel, delayed
 
 
 def t_estimate_integration():
     currentdir = os.getcwd()
     homefolder = os.path.dirname(currentdir)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    result_dir = 'result_v023_exp'
-=======
-    result_dir = 'result_v022_lin'
->>>>>>> parent of 9023769 (V022)
-=======
-    result_dir = 'result_v022_lin'
->>>>>>> parent of 9023769 (V022)
+    result_dir = 'result_v022_exp'
     data_temperature = '1896'
-    emissivity_set = '3'
+    emissivity_set = '2'
     data_name = 'T' + data_temperature + '_' + emissivity_set + '_digital'
     camera_folder = os.path.join(homefolder, 'program', 'camera_parameter')
 
@@ -46,8 +39,8 @@ def t_estimate_integration():
         intensity.append(pd.read_excel(os.path.join(experiment_folder, ('digital_value_' + data_temperature + '.xlsx')), 'channel_' + str(i), header=None))
     t_ref = np.array(pd.read_excel(os.path.join(experiment_folder, 't_field_' + data_temperature + '.xlsx'), header=None))
     intensity = np.array(intensity)
-    target = intensity#[:, 15:20, 15:20]
-    t_target = t_ref#[15:20, 15:20]
+    target = intensity
+    t_target = t_ref
     t_map = np.zeros((len(target[0]), len(target[0, 0])))
     Ea_map = np.zeros((len(target[0]), len(target[0, 0])))
     Eb_map = np.zeros((len(target[0]), len(target[0, 0])))
@@ -57,7 +50,7 @@ def t_estimate_integration():
     # parallel computing
     target_reshape = transfer_pos(target)
     print("Number of processors: ", mp.cpu_count())
-    cal_result = np.array(Parallel(n_jobs=mp.cpu_count()-1)(delayed(process_itg)(target_reshape[:, i], qe_array, tr_array) for i in range(len(target_reshape[0]))))
+    cal_result = np.array(Parallel(n_jobs=mp.cpu_count())(delayed(process_itg)(target_reshape[:, i], qe_array, tr_array) for i in range(len(target_reshape[0]))))
     t_map = transfer_neg(cal_result[:, 0], target)
     Ea_map = transfer_neg(cal_result[:, 1], target)
     Eb_map = transfer_neg(cal_result[:, 2], target)
@@ -88,8 +81,7 @@ def emissivity_average_cal(a, b):
 
 
 def lin_interpolation(x, x0, x1, y0, y1):
-    return np.interp(x, [x0, x1], [y0, y1])
-    # return y0+(y1-y0)*(x-x0)/(x1-x0)
+    return y0+(y1-y0)*(x-x0)/(x1-x0)
 
 
 def black_body_radiation(temperature, wavelength):
@@ -98,7 +90,7 @@ def black_body_radiation(temperature, wavelength):
     return param1/(wavelength**5)/(np.exp(param2/(wavelength*temperature))-1)
 
 
-def integration(wl,f_array,qe_array,a,b,t):
+def integration(wl, f_array, qe_array, a, b, t):
     wl0 = 0.5 * 10 ** (-6)
     wl1 = 1 * 10 ** (-6)
     f_i = len(f_array[0,f_array[0,:]*10**(-9)<=wl]) - 1
@@ -110,28 +102,27 @@ def integration(wl,f_array,qe_array,a,b,t):
     return result
 
 
+# def integration(wl, tr_array, qe_array, a, b, t):
+#     transparency = interp1d(tr_array[0], tr_array[1], kind='linear', fill_value='extrapolate')
+#     qe = interp1d(qe_array[0], qe_array[1], kind='linear', fill_value='extrapolate')
+#     result = emissivity_model(wl, a, b) * black_body_radiation(t, wl) * transparency(wl * (10**9)) * qe(wl * (10**9)) * 200
+#     return result
+
+
 def emissivity_model(wl, a, b):
     wl0 = 0.5 * 10 ** (-6)
     wl1 = 1 * 10 ** (-6)
     # lin emi = a + b * lambda
-    emissivity = a - b * (wl-wl0)/(wl1-wl0)   # a[0, 1], b[0, 1]
+    # emissivity = a - b * (wl-wl0)/(wl1-wl0)   # a[0, 1], b[0, 1]
 
     # lin exp emi = exp(a + b * lambda)
     # emissivity = math.exp(a + b * wl)
 
     # lin square emi = a + b * wl**2
-    # emissivity = a + b * (((wl-wl0)/(wl1-wl0)) ** 2)    # a[-2, 2], b[0, 1]
+    # emissivity = a + b * (wl**2)
 
     # exp emi = exp(-a - b * wl)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    emissivity = math.exp(-a - b * ((wl-wl0)/(wl1-wl0)))
-=======
-    # emissivity = math.exp(-a - b * wl)
->>>>>>> parent of 9023769 (V022)
-=======
-    # emissivity = math.exp(-a - b * wl)
->>>>>>> parent of 9023769 (V022)
+    emissivity = math.exp(-a - b * wl)
 
     # maxwell
     # emissivity = 4 * math.sqrt(a * (1 + math.sqrt(1 + (wl / b) ** 2))) / (2 * a * (1 + math.sqrt(1 + (wl / b) ** 2)) + 2 * math.sqrt(a * (1 + math.sqrt(1 + (wl / b) ** 2))) + 1)
@@ -150,27 +141,19 @@ def process_itg(intensity_array, qe_array, tr_array):
         return np.array(result_f)
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-<<<<<<< HEAD
-<<<<<<< HEAD
-        popt, cov = curve_fit(integration_solve, qe_array, intensity_array, bounds=((-2, 0, 500), (2, 1, 1958.2)), maxfev= 100000)
-=======
-        popt, cov = curve_fit(integration_solve, qe_array, intensity_array, bounds = ((0, 0, 500), (1, 1, 1958.2)), maxfev= 100000)
->>>>>>> parent of 9023769 (V022)
-=======
-        popt, cov = curve_fit(integration_solve, qe_array, intensity_array, bounds = ((0, 0, 500), (1, 1, 1958.2)), maxfev= 100000)
->>>>>>> parent of 9023769 (V022)
+        popt, cov = curve_fit(integration_solve, qe_array, intensity_array, bounds=((-50, -50, 500), (50, 50, 1958.2)), maxfev= 100000)
     return popt[2], popt[0], popt[1]
 
 
 def save_file(t_field, temperature_center, emissivity_set, emi_field, result_dir):
     dir_name = 'T' + str(temperature_center) + '_' + str(emissivity_set) + '_digital'
-    if not os.path.exists(os.path.join('results', result_dir)):
-        os.mkdir(os.path.join('results', result_dir))
-    if not os.path.exists(os.path.join('results', result_dir, dir_name)):
-        os.mkdir(os.path.join('results', result_dir, dir_name))
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
+    if not os.path.exists(os.path.join(result_dir, dir_name)):
+        os.mkdir(os.path.join(result_dir, dir_name))
 
     # t_field
-    file_t = os.path.join('results', result_dir, dir_name, 't_cal_' + str(temperature_center) + '.xlsx')
+    file_t = os.path.join(result_dir, dir_name, 't_cal_' + str(temperature_center) + '.xlsx')
     workbook_t = openpyxl.Workbook()
     worksheet_t = workbook_t.active
     plt.imshow(t_field, cmap='viridis')
@@ -178,7 +161,7 @@ def save_file(t_field, temperature_center, emissivity_set, emi_field, result_dir
     plt.xlabel('X_position')
     plt.ylabel('Y_position')
     plt.title('Temperature_map')
-    plt.savefig(os.path.join('results', result_dir, dir_name, 't_cal' + '.jpg'))
+    plt.savefig(os.path.join(result_dir, dir_name, 't_cal' + '.jpg'))
     plt.clf()
 
     for row in t_field:
@@ -186,7 +169,7 @@ def save_file(t_field, temperature_center, emissivity_set, emi_field, result_dir
     workbook_t.save(file_t)
 
     # emi_field
-    file_emi = os.path.join('results', result_dir, dir_name, 'emi_cal_' + str(temperature_center) + '.xlsx')
+    file_emi = os.path.join(result_dir, dir_name, 'emi_cal_' + str(temperature_center) + '.xlsx')
     workbook_emi = openpyxl.Workbook()
     worksheet_emi = workbook_emi.active
     plt.imshow(emi_field, cmap='viridis')
@@ -194,7 +177,7 @@ def save_file(t_field, temperature_center, emissivity_set, emi_field, result_dir
     plt.xlabel('X_position')
     plt.ylabel('Y_position')
     plt.title('Emissivity_map')
-    plt.savefig(os.path.join('results', result_dir, dir_name, 'emi_cal' + '.jpg'))
+    plt.savefig(os.path.join(result_dir, dir_name, 'emi_cal' + '.jpg'))
     plt.clf()
 
     for row in emi_field:
