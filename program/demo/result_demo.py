4def966a6164ba53531695d748d545a3c5d80721
Kleineryong <72655617+Kleineryong@users.noreply.github.com>
2023-08-07 14:13:41 +0200

    20230807 Thesis
    5.2.2 finish

--- program/demo/result_demo.py
+++ program/demo/result_demo.py
@@ -18,13 +18,13 @@ def result_demo():
     emi_model_save = ['mix', 'lin_square', 'linear', 'quad', 'exp']
 
     for i in range(len(emi_model)):
-        t_raw, emi_raw = read_result(os.path.join(result_dir, emi_model[i]))
-        save_result(t_raw, emi_raw, emi_model_save[i], save_dir)
+        t_raw, emi_raw, t_ori= read_result(os.path.join(result_dir, emi_model[i]))
+        save_result(t_raw, emi_raw, t_ori, emi_model_save[i], save_dir)
 
     return 0
 
 
-def save_result(t_raw, emi_raw, emi_model_name, save_dir):
+def save_result(t_raw, emi_raw, t_ori, emi_model_name, save_dir):
     dir_list = ['T1900_0_digital',
                 'T1900_5_digital', 'T1900_21_digital', 'T1900_22_digital', 'T1900_23_digital',
                 'T1900_24_digital', 'T1900_25_digital', 'T1900_26_digital', 'T1900_31_digital',
@@ -34,9 +34,9 @@ def save_result(t_raw, emi_raw, emi_model_name, save_dir):
         if not os.path.exists(os.path.join(save_dir, dir_save_list[i], emi_model_name)):
             os.mkdir(os.path.join(save_dir, dir_save_list[i], emi_model_name))
 
-        plt.figure(figsize=(8, 6), dpi=400)
+        plt.figure(figsize=(7, 6), dpi=400)
 
-        plt.imshow(t_raw[dir_list[i]], cmap='coolwarm')
+        plt.imshow(np.abs(t_raw[dir_list[i]]), cmap='coolwarm', vmin=0, vmax=0.1)
         cbar = plt.colorbar()
         plt.xlabel('X_position', fontsize=20)
         plt.ylabel('Y_position', fontsize=20)
@@ -46,15 +46,30 @@ def save_result(t_raw, emi_raw, emi_model_name, save_dir):
         plt.clf()
         plt.close()
 
-        plt.figure(figsize=(8, 6), dpi=400)
+        plt.figure(figsize=(7, 6), dpi=400)
 
-        plt.imshow(emi_raw[dir_list[i]], cmap='viridis')
+        plt.imshow(emi_raw[dir_list[i]], cmap='viridis', vmin=0, vmax=1)
         cbar = plt.colorbar()
         plt.xlabel('X_position', fontsize=20)
         plt.ylabel('Y_position', fontsize=20)
         plt.title('Emissivity', fontsize=20)
         plt.tight_layout()
         plt.savefig(os.path.join(save_dir, dir_save_list[i], emi_model_name, 'emi_cal.jpg'))
+        plt.clf()
+        plt.close()
+
+        plt.figure(figsize=(7, 6), dpi=400)
+        if dir_save_list[i] == '5':
+            v_min = 1500
+        else:
+            v_min = 1000
+        plt.imshow(np.abs(t_ori[dir_list[i]]), cmap='inferno', vmin=v_min, vmax=1900)
+        cbar = plt.colorbar()
+        plt.xlabel('X_position', fontsize=20)
+        plt.ylabel('Y_position', fontsize=20)
+        plt.title('Cal. temperature', fontsize=20)
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_dir, dir_save_list[i], emi_model_name, 'T_cal.jpg'))
         plt.clf()
         plt.close()
 
@@ -66,11 +81,13 @@ def read_result(result_dir):
                 'T1900_32_digital', 'T1900_33_digital', 'T1900_34_digital']
     t_raw = {}
     emi_raw = {}
+    t_ori = {}
     for result in dir_list:
         t_raw[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 't_bias.xlsx'), header=None))
         emi_raw[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 'emi_cal_1900.xlsx'), header=None))
+        t_ori[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 't_cal_1900.xlsx'), header=None))
         t_raw[result] = -1 * t_raw[result]
-    return t_raw, emi_raw
+    return t_raw, emi_raw, t_ori
 
 
 if 1:
