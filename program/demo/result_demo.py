4be1df44dc0d43b6a64858cf9048434d40d7a7bf
Kleineryong <72655617+Kleineryong@users.noreply.github.com>
2023-08-12 19:32:58 +0200

    20230812 thesis
    limitation and boundaries

--- program/demo/result_demo.py
+++ program/demo/result_demo.py
@@ -14,8 +14,8 @@ def result_demo():
     result_dir = os.path.join(homefolder, 'results')
     save_dir = 'F:\\Uni_Files\\Semesterarbeit\\Thesis\\LBAMStudentThesis\\figures\\raw_data'
 
-    emi_model = ['result_v040_lin_square_exp', 'result_v030_lin_square', 'result_v024_lin', 'result_v024_lin_square', 'result_v024_exp']
-    emi_model_save = ['mix', 'lin_square', 'linear', 'quad', 'exp']
+    emi_model = ['result_v060_lin_square', 'result_v060_lin', 'result_v060_exp']
+    emi_model_save = ['lin_square', 'linear', 'exp']
 
     for i in range(len(emi_model)):
         t_raw, emi_raw, t_ori= read_result(os.path.join(result_dir, emi_model[i]))
@@ -25,14 +25,15 @@ def result_demo():
 
 
 def save_result(t_raw, emi_raw, t_ori, emi_model_name, save_dir):
-    dir_list = ['T1900_0_digital',
-                'T1900_5_digital', 'T1900_21_digital', 'T1900_22_digital', 'T1900_23_digital',
-                'T1900_24_digital', 'T1900_25_digital', 'T1900_26_digital', 'T1900_31_digital',
-                'T1900_32_digital', 'T1900_33_digital', 'T1900_34_digital']
-    dir_save_list = ['0', '5', '21', '22', '23', '24', '25', '26', '31', '32', '33', '34']
+    dir_list = ['T3500_0_digital',
+                'T3500_5_digital', 'T3500_21_digital', 'T3500_22_digital',
+                'T3500_32_digital', 'T3500_33_digital']
+    dir_save_list = ['0', '5', '21', '22', '32', '33']
     for i in range(len(dir_save_list)):
-        if not os.path.exists(os.path.join(save_dir, dir_save_list[i], emi_model_name)):
-            os.mkdir(os.path.join(save_dir, dir_save_list[i], emi_model_name))
+        if not os.path.exists(os.path.join(save_dir, dir_save_list[i], "T3500")):
+            os.mkdir(os.path.join(save_dir, dir_save_list[i], "T3500"))
+        if not os.path.exists(os.path.join(save_dir, dir_save_list[i], "T3500", emi_model_name)):
+            os.mkdir(os.path.join(save_dir, dir_save_list[i], "T3500", emi_model_name))
 
         plt.figure(figsize=(7, 6), dpi=400)
 
@@ -42,7 +43,7 @@ def save_result(t_raw, emi_raw, t_ori, emi_model_name, save_dir):
         plt.ylabel('Y_position', fontsize=20)
         plt.title('Rel. temperature difference', fontsize=20)
         plt.tight_layout()
-        plt.savefig(os.path.join(save_dir, dir_save_list[i], emi_model_name, 'T_bias.jpg'))
+        plt.savefig(os.path.join(save_dir, dir_save_list[i], "T3500", emi_model_name, 'T_bias.jpg'))
         plt.clf()
         plt.close()
 
@@ -54,38 +55,33 @@ def save_result(t_raw, emi_raw, t_ori, emi_model_name, save_dir):
         plt.ylabel('Y_position', fontsize=20)
         plt.title('Emissivity', fontsize=20)
         plt.tight_layout()
-        plt.savefig(os.path.join(save_dir, dir_save_list[i], emi_model_name, 'emi_cal.jpg'))
+        plt.savefig(os.path.join(save_dir, dir_save_list[i], "T3500", emi_model_name, 'emi_cal.jpg'))
         plt.clf()
         plt.close()
 
         plt.figure(figsize=(7, 6), dpi=400)
-        if dir_save_list[i] == '5':
-            v_min = 1500
-        else:
-            v_min = 1000
-        plt.imshow(np.abs(t_ori[dir_list[i]]), cmap='inferno', vmin=v_min, vmax=1900)
+        plt.imshow(np.abs(t_ori[dir_list[i]]), cmap='inferno', vmin=2500, vmax=3500)
         cbar = plt.colorbar()
         plt.xlabel('X_position', fontsize=20)
         plt.ylabel('Y_position', fontsize=20)
         plt.title('Cal. temperature', fontsize=20)
         plt.tight_layout()
-        plt.savefig(os.path.join(save_dir, dir_save_list[i], emi_model_name, 'T_cal.jpg'))
+        plt.savefig(os.path.join(save_dir, dir_save_list[i], "T3500", emi_model_name, 'T_cal.jpg'))
         plt.clf()
         plt.close()
 
 
 def read_result(result_dir):
-    dir_list = ['T1900_0_digital',
-                'T1900_5_digital', 'T1900_21_digital', 'T1900_22_digital', 'T1900_23_digital',
-                'T1900_24_digital', 'T1900_25_digital', 'T1900_26_digital', 'T1900_31_digital',
-                'T1900_32_digital', 'T1900_33_digital', 'T1900_34_digital']
+    dir_list = ['T3500_0_digital',
+                'T3500_5_digital', 'T3500_21_digital', 'T3500_22_digital',
+                'T3500_32_digital', 'T3500_33_digital']
     t_raw = {}
     emi_raw = {}
     t_ori = {}
     for result in dir_list:
         t_raw[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 't_bias.xlsx'), header=None))
-        emi_raw[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 'emi_cal_1900.xlsx'), header=None))
-        t_ori[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 't_cal_1900.xlsx'), header=None))
+        emi_raw[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 'emi_cal_3500.xlsx'), header=None))
+        t_ori[result] = np.array(pd.read_excel(os.path.join(result_dir, result, 't_cal_3500.xlsx'), header=None))
         t_raw[result] = -1 * t_raw[result]
     return t_raw, emi_raw, t_ori
 
