caa9071e857d274fcbc7f8243b44921eaf5e44f1
Kleineryong <72655617+Kleineryong@users.noreply.github.com>
2023-08-25 20:20:26 +0200

    thesis v2.0

--- matlab_program/data_reading.m
+++ matlab_program/data_reading.m
@@ -1,6 +1,10 @@
 clear
 
-% address = 'V0_bb_1085/T1085_100_20220909_145637_170_00000_channel0.tiff';
-address = 'data/T1700_data3_norm/T1700_channel_548.tiff';
+address = 'V0_bb_850/T850_1000_20220909_141416_613_00000_channel0.tiff';
+% address = 'data/T1700_data3_norm/T1700_channel_548.tiff';
 data = imread(address);
 info = imfinfo(address);
+
+x = 
+
+plot(data[80, :], );
